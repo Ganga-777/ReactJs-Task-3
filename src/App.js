75597ae041f2c6d9e2c49f5856174fec825ec92e
Ganga-777 <144657923+Ganga-777@users.noreply.github.com>
2023-10-14 12:28:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ColorPicker from './ColorPicker';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const colors = ['#fe0000', '#00ff01', '#0000fe', '#ffff00', '#ff00fe', '#00ffff', '#fea500', '#81007f', '#ffc0cb', '#008001', '#fe6347', '#01ced1', '#8b4512', '#ff8b00', '#4682b4', '#fed700'];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Other content */}
+        <ColorPicker colors={colors} />
+      </header>
+    </div>
+  );
+};
+
+export default App;
